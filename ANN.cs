b36a2d222847526f38ed0fc3fd264810ff51e4c6
Kevin Silvester <kevinsilvester05@gmail.com>
2024-10-09 12:14:22 +0100

    test: added test asssets

---
+++ ANN.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestSharp;
+
+namespace ann_jikan
+{
+    public class ANN
+    {
+        private static readonly RestClient ANN_jClient = new RestClient("https://cdn.animenewsnetwork.com/encyclopedia/api.xml");
+    }
+}
